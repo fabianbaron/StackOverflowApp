5d5e4adcb9daaa0e23b36c7f55810cd94c8f6019
Fabian <fabian.baron@gmail.com>
2018-09-12 22:38:54 -0500

    Working. Developement: Retrofit + Recycler View + Butterknife

--- app/src/main/java/com/example/fabia/stackoverflowapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/fabia/stackoverflowapp/RecyclerViewAdapter.java
@@ -14,6 +14,7 @@ import butterknife.ButterKnife;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
+
     private List<Answers> mDatos;
 
     public class ViewHolder extends RecyclerView.ViewHolder {
